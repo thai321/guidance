74f77d17beb19072c259b085f92796b68451b332
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-09-19 18:05:05 -0700

    Add Projects model, seed users and projects

---
+++ Guidance/db/migrate/20170920001754_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration[5.1]
+  def change
+    create_table :projects do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :image_url, null: false
+      t.string :video_url, null: false
+      t.boolean :published, null: false, default: false
+      t.integer :author_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :projects, :title
+    add_index :projects, :author_id
+  end
+end
--- Guidance/db/seeds.rb
+++ Guidance/db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Project.destroy_all
+
+# Users
+
+u0 = User.create(username: 'cat0', email: 'cat0@gmail.com', password: '123456')
+u1 = User.create(username: 'cat1', email: 'cat1@gmail.com', password: '123456')
+u2 = User.create(username: 'cat2', email: 'cat2@gmail.com', password: '123456')
+u3 = User.create(username: 'cat3', email: 'cat3@gmail.com', password: '123456')
+u4 = User.create(username: 'cat4', email: 'cat4@gmail.com', password: '123456')
+u5 = User.create(username: 'cat5', email: 'cat5@gmail.com', password: '123456')
+u6 = User.create(username: 'cat6', email: 'cat6@gmail.com', password: '123456')
+u7 = User.create(username: 'cat7', email: 'cat7@gmail.com', password: '123456')
+
+puts "Create 8 users"
+
+# projects
+
+p0 = Project.create(title: 'Project0', description: 'Project0 description', image_url: 'image0', video_url: 'video0', author_id: u0.id)
+p1 = Project.create(title: 'Project1', description: 'Project1 description', image_url: 'image1', video_url: 'video1', author_id: u1.id)
+p2 = Project.create(title: 'Project2', description: 'Project2 description', image_url: 'image2', video_url: 'video2', author_id: u2.id)
+p3 = Project.create(title: 'Project3', description: 'Project3 description', image_url: 'image3', video_url: 'video3', author_id: u3.id)
+p4 = Project.create(title: 'Project4', description: 'Project4 description', image_url: 'image4', video_url: 'video4', author_id: u4.id)
+p5 = Project.create(title: 'Project5', description: 'Project5 description', image_url: 'image5', video_url: 'video5', author_id: u5.id)
+p6 = Project.create(title: 'Project6', description: 'Project6 description', image_url: 'image6', video_url: 'video6', author_id: u6.id)
+p7 = Project.create(title: 'Project7', description: 'Project7 description', image_url: 'image7', video_url: 'video7', author_id: u7.id)
+
+puts "Create 8 projects"
