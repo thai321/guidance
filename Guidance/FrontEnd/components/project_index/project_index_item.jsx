a15ca0f6022eef0e08347e2edb50f4a3e350a8ac
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-09-20 17:25:01 -0700

    Style project-form page

--- Guidance/FrontEnd/components/project_index/project_index_item.jsx
+++ Guidance/FrontEnd/components/project_index/project_index_item.jsx
@@ -6,15 +6,22 @@ const ProjectIndexItem = ({ project }) => {
 
   return (
     <div className="col col-md-3">
-      <li>
-        <Link to={`/projects/${project.id}`}>
-          <h2>{image_url}</h2>
-          <img src="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" />
-          <h3>
-            {title} By {author_id}
-          </h3>
-        </Link>
-      </li>
+      <div class="thumbnail">
+        <img src="http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg" />
+        <div class="caption">
+          <h3>{title}</h3>
+          <h5>by {author_id}</h5>
+
+          <p>
+            <Link
+              to={`/projects/${project.id}`}
+              className="btn btn-info btn-block"
+            >
+              View
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- Guidance/Gemfile
+++ Guidance/Gemfile
@@ -34,6 +34,8 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'bootstrap', '~> 4.0.0.beta'
 # gem 'rails_12factor'
+gem "font-awesome-rails"
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
