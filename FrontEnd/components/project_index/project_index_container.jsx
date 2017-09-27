5e56a7e962becb09e3541e7acdc1ba510fc40b15
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-09-27 08:44:03 -0700

    user now can search for projects

--- FrontEnd/components/project_index/project_index_container.jsx
+++ FrontEnd/components/project_index/project_index_container.jsx
@@ -5,10 +5,17 @@ import { fetchProject, fetchProjects } from '../../actions/project_actions';
 import ProjectIndex from './project_index';
 
 const mapStateToProps = state => {
+  const projects = [];
+
+  const projectIds = Object.values(state.entities.projects.allIds);
+
+  projectIds.forEach(id => {
+    projects.push(state.entities.projects[id]);
+  });
+
   return {
-    projectIds: Object.values(state.entities.projects.allIds),
-    projects: state.entities.projects,
-    currentUser: state.session.currentUser
+    currentUser: state.session.currentUser,
+    projects
   };
 };
 
