9b572ed755901b814bd2397f9c339ac4102062c8
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-18 00:46:41 -0700

    to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
@@ -22,5 +23,10 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps'
+  devtool: 'source-maps',
+  plugins: [
+   new webpack.DefinePlugin({
+     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+   }),
+ ]
 };
