f1b1c4cc029645c27276bf5198fca49ed7114ed0
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-10-21 23:53:45 -0700

    Style the footer to icons instead of button

--- Guidance/FrontEnd/components/footer/footer.jsx
+++ Guidance/FrontEnd/components/footer/footer.jsx
@@ -5,61 +5,31 @@ import { Link } from 'react-router-dom';
 class Footer extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+      <ul className="footer_container">
+        <li className="nav-item">
+          <a href="https://github.com/thai321/guidance">
+            <i className="fa fa-code-fork fa-4x" aria-hidden="true" />
+          </a>
+        </li>
 
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a
-                  className="nav-link btn btn-outline-success"
-                  href="https://www.github.com/thai321/guidance"
-                >
-                  Source
-                </a>
-              </li>
+        <li className="nav-item">
+          <a href="https://github.com/thai321">
+            <i className="fa fa-github fa-4x" aria-hidden="true" />
+          </a>
+        </li>
 
-              <li className="nav-item">
-                <a
-                  className="nav-link btn btn-outline-primary"
-                  href="https://www.github.com/thai321"
-                >
-                  Github
-                </a>
-              </li>
+        <li className="nav-item">
+          <a href="https://www.linkedin.com/in/thai-nguyen91/">
+            <i className="fa fa-linkedin-square fa-4x" aria-hidden="true" />
+          </a>
+        </li>
 
-              <li className="nav-item">
-                <a
-                  className="nav-link btn btn-outline-warning"
-                  href="https://www.linkedin.com/in/thai-nguyen91/"
-                >
-                  Linkedin
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <a
-                  className="nav-link btn btn-outline-info"
-                  href="http://www.qthai.me/"
-                >
-                  Thai Nguyen
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+        <li className="nav-item">
+          <a href="http://www.qthai.me/">
+            <i className="fa fa-user fa-4x" aria-hidden="true" />
+          </a>
+        </li>
+      </ul>
     );
   }
 }
