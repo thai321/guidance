46358748a7d5b02b24f8b659ab9576aba8dbe76d
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-11-07 22:15:19 -0800

    Fix user-show-page style for bio

--- Guidance/webpack.config.js
+++ Guidance/webpack.config.js
@@ -1,27 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  context: __dirname,
-  entry: './FrontEnd/guidance.jsx',
-  output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
+new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+}),
+  (module.exports = {
+    context: __dirname,
+    entry: './FrontEnd/guidance.jsx',
+    output: {
+      path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015']
+          }
         }
-      }
-    ]
-  },
-  devtool: 'source-maps',
-};
+      ]
+    },
+    devtool: 'source-maps'
+  });
